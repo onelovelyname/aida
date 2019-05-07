3cd133b91c4425007af2cb6a3c5ff76fc60280bf
Melanie Gin <mg@carefreebreaks.com>
2019-05-07 15:11:54 +0300

    Update CSS and JS files to be minified

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,21 +1,30 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
-
-// Task to minify css using package cleanCSs
-gulp.task('minify-css', () => {
-  // Folder with files to minify
-  return gulp.src('public/stylesheets/*.css')
-  //The method pipe() allow you to chain multiple tasks together
-  //I execute the task to minify the files
-    .pipe(cleanCSS())
-    //I define the destination of the minified files with the method dest
-    .pipe(gulp.dest('public/dist'));
-});
+let uglify = require('gulp-uglify');
+var purify = require('gulp-purifycss');
+let pipeline = require('readable-stream').pipeline;
 
 //We create a 'default' task that will run when we run `gulp` in the project
 gulp.task('default', function() {
 // We use `gulp.watch` for Gulp to expect changes in the files to run again
   gulp.watch('./public/stylesheets/*.css', function(evt) {
+    gulp.task('css');
     gulp.task('minify-css');
+    gulp.task('compress');
   });
+});
+
+gulp.task('css', function() {
+  return gulp.src('./public/src/stylesheets/*.css')
+    .pipe(purify(['./public/src/javascripts/*.js', './views/*.pug', './views/**/*.pug']))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./public/dist/stylesheets'));
+});
+
+gulp.task('compress', function () {
+  return pipeline(
+    gulp.src('public/src/javascripts/*.js'),
+    uglify(),
+    gulp.dest('public/dist/javascripts')
+  );
 });
